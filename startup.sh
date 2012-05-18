e69a175f4c59be0c253ba41b5f101f2c03092359
Stas Boukarev <stassats@gmail.com>
2012-05-18 04:30:40 +0400

    Actually commit startup.sh.

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ulimit -t 86400
+ulimit -l 1572864 # 86400s and 1572864kB
+
+export SBCL_HOME=~/sbcl-new/contrib/
+
+while ! sh ~/sbcl-new/run-sbcl.sh --dynamic-space-size 1024  \
+    --lose-on-corruption \
+    --disable-ldb \
+    --userinit ~/ql-sbclrc \
+    --disable-debugger \
+    --load "start-bots.lisp"
+    do
+    sleep 5
+done
